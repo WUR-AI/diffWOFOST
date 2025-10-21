a4bd4bf2ed6f83a298d62e09bcf26e29d8354ec9
SarahAlidoost <f.alidoost@esciencecenter.nl>
2025-10-21 11:25:30 +0200

    fix init

--- src/diffwofost/__init__.py
+++ src/diffwofost/__init__.py
@@ -1,9 +1,18 @@
 """Documentation about diffwofost."""
 
 import logging
+from diffwofost.physical_models.crop import leaf_dynamics
+from diffwofost.physical_models.crop import root_dynamics
+from diffwofost.physical_models import utils
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = ""
 __email__ = ""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
+
+__all__ = [
+    "leaf_dynamics",
+    "root_dynamics",
+    "utils",
+]
