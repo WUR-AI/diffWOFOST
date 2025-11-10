347a53647fcb29fcfc6f45935741dc1f8f98ea7f
SCiarella <58949181+SCiarella@users.noreply.github.com>
2025-11-10 11:10:53 +0100

    Make Root module fully differentiable (#55)
    * Make root module differentiable
    
    * Vectorize root module
    
    * Introduce differentiable Afgen class
    
    * Update tests
    
    * Implement pre-commit hook
    
    ---------
    
    Co-authored-by: SarahAlidoost <55081872+SarahAlidoost@users.noreply.github.com>

--- tests/physical_models/crop/test_root_dynamics.py
+++ tests/physical_models/crop/test_root_dynamics.py
@@ -1,4 +1,5 @@
 import copy
+import warnings
 from unittest.mock import patch
 import pytest
 import torch
@@ -11,6 +12,9 @@ from diffwofost.physical_models.utils import calculate_numerical_grad
 from diffwofost.physical_models.utils import get_test_data
 from diffwofost.physical_models.utils import prepare_engine_input
 from .. import phy_data_folder
+
+# Ignore deprecation warnings from pcse.base.simulationobject
+pytestmark = pytest.mark.filterwarnings("ignore::DeprecationWarning:pcse.base.simulationobject")
 
 
 def get_test_diff_root_model():
@@ -69,7 +73,7 @@ class TestRootDynamics:
 
         # prepare model input
         test_data_path = phy_data_folder / "test_rootdynamics_wofost72_01.yaml"
-        crop_model_params = ["RDI", "RRI", "RDMCR", "RDMSOL", "TDWI", "IAIRDU"]
+        crop_model_params = ["RDI", "RRI", "RDMCR", "RDMSOL", "TDWI", "IAIRDU", "RDRRTB"]
         (
             crop_model_params_provider,
             weather_data_provider,
@@ -103,7 +107,7 @@ class TestRootDynamics:
     def test_root_dynamics_with_engine(self):
         # prepare model input
         test_data_path = phy_data_folder / "test_rootdynamics_wofost72_01.yaml"
-        crop_model_params = ["RDI", "RRI", "RDMCR", "RDMSOL", "TDWI", "IAIRDU"]
+        crop_model_params = ["RDI", "RRI", "RDMCR", "RDMSOL", "TDWI", "IAIRDU", "RDRRTB"]
         (crop_model_params_provider, weather_data_provider, agro_management_inputs, _) = (
             prepare_engine_input(test_data_path, crop_model_params)
         )
@@ -119,10 +123,221 @@ class TestRootDynamics:
                 config_path,
             )
 
+    @pytest.mark.parametrize("param", ["RDI", "RRI", "RDMCR", "RDMSOL", "TDWI", "IAIRDU", "RDRRTB"])
+    def test_root_dynamics_with_one_parameter_vector(self, param):
+        # prepare model input
+        test_data_path = phy_data_folder / "test_rootdynamics_wofost72_01.yaml"
+        crop_model_params = ["RDI", "RRI", "RDMCR", "RDMSOL", "TDWI", "IAIRDU", "RDRRTB"]
+        (
+            crop_model_params_provider,
+            weather_data_provider,
+            agro_management_inputs,
+            external_states,
+        ) = prepare_engine_input(test_data_path, crop_model_params)
+        config_path = str(phy_data_folder / "WOFOST_Root_Dynamics.conf")
+
+        # Setting a vector (with one value) for the selected parameter
+        # If the parameter is an Afgen table (like RDRRTB), the repeat will create a
+        # tensor of Afgen objects
+        if param == "RDRRTB":
+            repeated = crop_model_params_provider[param].repeat(10, 1)
+        else:
+            repeated = crop_model_params_provider[param].repeat(10)
+        crop_model_params_provider.set_override(param, repeated, check=False)
+
+        engine = EngineTestHelper(
+            crop_model_params_provider,
+            weather_data_provider,
+            agro_management_inputs,
+            config_path,
+            external_states,
+        )
+        engine.run_till_terminate()
+        actual_results = engine.get_output()
+
+        # get expected results from YAML test data
+        expected_results, expected_precision = get_test_data(test_data_path)
+
+        assert len(actual_results) == len(expected_results)
+
+        for reference, model in zip(expected_results, actual_results, strict=False):
+            assert reference["DAY"] == model["day"]
+            assert all(
+                all(abs(reference[var] - model[var]) < precision)
+                for var, precision in expected_precision.items()
+            )
+
+    @pytest.mark.parametrize(
+        "param,delta",
+        [
+            ("RDI", 1.0),
+            ("RRI", 0.1),
+        ],
+    )
+    def test_root_dynamics_with_different_parameter_values(self, param, delta):
+        # prepare model input
+        test_data_path = phy_data_folder / "test_rootdynamics_wofost72_01.yaml"
+        crop_model_params = ["RDI", "RRI", "RDMCR", "RDMSOL", "TDWI", "IAIRDU", "RDRRTB"]
+        (
+            crop_model_params_provider,
+            weather_data_provider,
+            agro_management_inputs,
+            external_states,
+        ) = prepare_engine_input(test_data_path, crop_model_params)
+        config_path = str(phy_data_folder / "WOFOST_Root_Dynamics.conf")
+
+        # Setting a vector with multiple values for the selected parameter
+        test_value = crop_model_params_provider[param]
+        # We set the value for which test data are available as the last element
+        param_vec = torch.tensor([test_value - delta, test_value + delta, test_value])
+        crop_model_params_provider.set_override(param, param_vec, check=False)
+
+        engine = EngineTestHelper(
+            crop_model_params_provider,
+            weather_data_provider,
+            agro_management_inputs,
+            config_path,
+            external_states,
+        )
+        engine.run_till_terminate()
+        actual_results = engine.get_output()
+
+        # get expected results from YAML test data
+        expected_results, expected_precision = get_test_data(test_data_path)
+
+        assert len(actual_results) == len(expected_results)
+
+        for reference, model in zip(expected_results, actual_results, strict=False):
+            assert reference["DAY"] == model["day"]
+            assert all(
+                # The value for which test data are available is the last element
+                abs(reference[var] - model[var][-1]) < precision
+                for var, precision in expected_precision.items()
+            )
+
+    def test_root_dynamics_with_multiple_parameter_vectors(self):
+        # prepare model input
+        test_data_path = phy_data_folder / "test_rootdynamics_wofost72_01.yaml"
+        crop_model_params = ["RDI", "RRI", "RDMCR", "RDMSOL", "TDWI", "IAIRDU", "RDRRTB"]
+        (
+            crop_model_params_provider,
+            weather_data_provider,
+            agro_management_inputs,
+            external_states,
+        ) = prepare_engine_input(test_data_path, crop_model_params)
+        config_path = str(phy_data_folder / "WOFOST_Root_Dynamics.conf")
+
+        # Setting a vector (with one value) for the RDI and RRI parameters
+        for param in ("RDI", "RRI", "RDMCR", "RDMSOL", "TDWI", "IAIRDU", "RDRRTB"):
+            # If the parameter is an Afgen table (like RDRRTB), the repeat will create a
+            # tensor of Afgen objects
+            if param == "RDRRTB":
+                repeated = crop_model_params_provider[param].repeat(10, 1)
+            else:
+                repeated = crop_model_params_provider[param].repeat(10)
+            crop_model_params_provider.set_override(param, repeated, check=False)
+
+        engine = EngineTestHelper(
+            crop_model_params_provider,
+            weather_data_provider,
+            agro_management_inputs,
+            config_path,
+            external_states,
+        )
+        engine.run_till_terminate()
+        actual_results = engine.get_output()
+
+        # get expected results from YAML test data
+        expected_results, expected_precision = get_test_data(test_data_path)
+
+        assert len(actual_results) == len(expected_results)
+
+        for reference, model in zip(expected_results, actual_results, strict=False):
+            assert reference["DAY"] == model["day"]
+            assert all(
+                all(abs(reference[var] - model[var]) < precision)
+                for var, precision in expected_precision.items()
+            )
+
+    def test_root_dynamics_with_multiple_parameter_arrays(self):
+        # prepare model input
+        test_data_path = phy_data_folder / "test_rootdynamics_wofost72_01.yaml"
+        crop_model_params = ["RDI", "RRI", "RDMCR", "RDMSOL", "TDWI", "IAIRDU", "RDRRTB"]
+        (
+            crop_model_params_provider,
+            weather_data_provider,
+            agro_management_inputs,
+            external_states,
+        ) = prepare_engine_input(test_data_path, crop_model_params)
+        config_path = str(phy_data_folder / "WOFOST_Root_Dynamics.conf")
+
+        # Setting an array with arbitrary shape (and one value)
+        for param in ("RDI", "RRI", "RDMCR", "RDMSOL", "TDWI", "IAIRDU", "RDRRTB"):
+            if param == "RDRRTB":
+                repeated = crop_model_params_provider[param].repeat((30, 5, 1))
+            else:
+                repeated = crop_model_params_provider[param].broadcast_to((30, 5))
+            crop_model_params_provider.set_override(param, repeated, check=False)
+
+        engine = EngineTestHelper(
+            crop_model_params_provider,
+            weather_data_provider,
+            agro_management_inputs,
+            config_path,
+            external_states,
+        )
+        engine.run_till_terminate()
+        actual_results = engine.get_output()
+
+        # get expected results from YAML test data
+        expected_results, expected_precision = get_test_data(test_data_path)
+
+        assert len(actual_results) == len(expected_results)
+
+        for reference, model in zip(expected_results, actual_results, strict=False):
+            assert reference["DAY"] == model["day"]
+            assert all(
+                torch.all(abs(reference[var] - model[var]) < precision)
+                for var, precision in expected_precision.items()
+            )
+            assert all(
+                model[var].shape == (30, 5) for var in expected_precision.keys()
+            )  # check the output shapes
+
+    def test_root_dynamics_with_incompatible_parameter_vectors(self):
+        # prepare model input
+        test_data_path = phy_data_folder / "test_rootdynamics_wofost72_01.yaml"
+        crop_model_params = ["RDI", "RRI", "RDMCR", "RDMSOL", "TDWI", "IAIRDU", "RDRRTB"]
+        (
+            crop_model_params_provider,
+            weather_data_provider,
+            agro_management_inputs,
+            external_states,
+        ) = prepare_engine_input(test_data_path, crop_model_params)
+        config_path = str(phy_data_folder / "WOFOST_Root_Dynamics.conf")
+
+        # Setting a vector (with one value) for the RDI and RRI parameters,
+        # but with different lengths
+        crop_model_params_provider.set_override(
+            "RDI", crop_model_params_provider["RDI"].repeat(10), check=False
+        )
+        crop_model_params_provider.set_override(
+            "RRI", crop_model_params_provider["RRI"].repeat(5), check=False
+        )
+
+        with pytest.raises(AssertionError):
+            EngineTestHelper(
+                crop_model_params_provider,
+                weather_data_provider,
+                agro_management_inputs,
+                config_path,
+                external_states,
+            )
+
     def test_wofost_pp_with_root_dynamics(self):
         # prepare model input
         test_data_path = phy_data_folder / "test_potentialproduction_wofost72_01.yaml"
-        crop_model_params = ["RDI", "RRI", "RDMCR", "RDMSOL", "TDWI", "IAIRDU"]
+        crop_model_params = ["RDI", "RRI", "RDMCR", "RDMSOL", "TDWI", "IAIRDU", "RDRRTB"]
         (crop_model_params_provider, weather_data_provider, agro_management_inputs, _) = (
             prepare_engine_input(test_data_path, crop_model_params)
         )
@@ -147,53 +362,118 @@ class TestRootDynamics:
                 )
 
 
-class TestDiffRootDynamicsTDWI:
-    def test_gradients_tdwi_rd_root_dynamics(self):
+class TestDiffRootDynamicsGradients:
+    """Parametrized tests for gradient calculations in root dynamics."""
+
+    # Define parameters and outputs
+    param_names = ["TDWI", "RDI", "RRI", "RDMCR", "RDMSOL", "IAIRDU", "RDRRTB"]
+    output_names = ["RD", "TWRT"]
+
+    # Define parameter configurations (value, dtype)
+    param_configs = {
+        "single": {
+            "TDWI": (0.2, torch.float32),
+            "RDI": (10.1, torch.float32),
+            "RRI": (2.25, torch.float32),
+            "RDMCR": (121, torch.float32),
+            "RDMSOL": (121, torch.float32),
+            "IAIRDU": (0.2, torch.float32),
+            "RDRRTB": ([0.0, 0.0, 1.5, 0.02], torch.float32),
+        },
+        "tensor": {
+            "TDWI": ([0.2, 0.3, 0.5], torch.float32),
+            "RDI": ([10, 10.1], torch.float32),
+            "RRI": ([1.0, 1.5, 2.0, 2.25], torch.float32),
+            "RDMCR": ([120, 121], torch.float32),
+            "RDMSOL": ([120, 121], torch.float32),
+            "IAIRDU": ([0.2, 0.2, 0.3, 0.4], torch.float32),
+            "RDRRTB": ([[0.0, 0.0, 1.5, 0.02], [0.0, 0.0, 1.6, 0.03]], torch.float32),
+        },
+    }
+
+    # Define which parameter-output pairs should have gradients
+    # Format: {param_name: [list of outputs that should have gradients]}
+    gradient_mapping = {
+        "TDWI": ["TWRT"],  # e.g. TDWI affects TWRT so I put it here
+        "RDI": ["RD"],
+        "RRI": ["RD"],
+        "RDMCR": ["RD"],
+        "RDMSOL": ["RD"],
+        "RDRRTB": ["TWRT"],
+    }
+
+    # Generate all combinations
+    gradient_params = []
+    no_gradient_params = []
+    for param_name in param_names:
+        for output_name in output_names:
+            if output_name in gradient_mapping.get(param_name, []):
+                gradient_params.append((param_name, output_name))
+            else:
+                no_gradient_params.append((param_name, output_name))
+
+    @pytest.mark.parametrize("param_name,output_name", no_gradient_params)
+    @pytest.mark.parametrize("config_type", ["single", "tensor"])
+    def test_no_gradients(self, param_name, output_name, config_type):
+        """Test cases where parameters should not have gradients for specific outputs."""
         model = get_test_diff_root_model()
-        tdwi = torch.nn.Parameter(torch.tensor(0.2, dtype=torch.float32))
-        output = model({"TDWI": tdwi})
-        rd = output["RD"]
-        loss = rd.sum()
+        value, dtype = self.param_configs[config_type][param_name]
+        param = torch.nn.Parameter(torch.tensor(value, dtype=dtype))
+        output = model({param_name: param})
+        loss = output[output_name].sum()
 
-        assert loss.grad_fn is None  # tdwi does not contribute to rd
+        assert loss.grad_fn is None
 
-    def test_gradients_tdwi_twrt_root_dynamics(self):
-        # prepare model input
+    @pytest.mark.parametrize("param_name,output_name", gradient_params)
+    @pytest.mark.parametrize("config_type", ["single", "tensor"])
+    def test_gradients_forward_backward_match(self, param_name, output_name, config_type):
+        """Test that forward and backward gradients match for parameter-output pairs."""
         model = get_test_diff_root_model()
-        tdwi = torch.nn.Parameter(torch.tensor(0.2, dtype=torch.float32))
-        output = model({"TDWI": tdwi})
-        twrt = output["TWRT"]
-        loss = twrt.sum()
+        value, dtype = self.param_configs[config_type][param_name]
+        param = torch.nn.Parameter(torch.tensor(value, dtype=dtype))
+        output = model({param_name: param})
+        loss = output[output_name].sum()
 
-        # this is ∂loss/∂tdwi
+        # this is ∂loss/∂param
         # this is called forward gradient here because it is calculated without backpropagation.
-        grads = torch.autograd.grad(loss, tdwi, retain_graph=True)[0]
+        grads = torch.autograd.grad(loss, param, retain_graph=True)[0]
 
-        assert grads is not None, "Gradients for TDWI should not be None"
+        assert grads is not None, f"Gradients for {param_name} should not be None"
 
-        tdwi.grad = None  # clear any existing gradient
+        param.grad = None  # clear any existing gradient
         loss.backward()
 
-        # this is ∂loss/∂tdwi calculated using backpropagation
-        grad_backward = tdwi.grad
+        # this is ∂loss/∂param calculated using backpropagation
+        grad_backward = param.grad
 
-        assert grad_backward is not None, "Backward gradients for TDWI should not be None"
-        assert grad_backward == grads, "Forward and backward gradients for TDWI should match"
+        assert grad_backward is not None, f"Backward gradients for {param_name} should not be None"
+        assert torch.allclose(grad_backward, grads), (
+            f"Forward and backward gradients for {param_name} should match"
+        )
 
-    def test_gradients_tdwi_twrt_root_dynamics_numerical(self):
-        tdwi = torch.nn.Parameter(torch.tensor(0.2, dtype=torch.float64))
-        output_name = "TWRT"  # Index 1 is "TWRT"
+    @pytest.mark.parametrize("param_name,output_name", gradient_params)
+    @pytest.mark.parametrize("config_type", ["single", "tensor"])
+    def test_gradients_numerical(self, param_name, output_name, config_type):
+        """Test that analytical gradients match numerical gradients."""
+        value, _ = self.param_configs[config_type][param_name]
+        param = torch.nn.Parameter(torch.tensor(value, dtype=torch.float64))
         numerical_grad = calculate_numerical_grad(
-            get_test_diff_root_model, "TDWI", tdwi.data, output_name
+            get_test_diff_root_model, param_name, param.data, output_name
         )
 
         model = get_test_diff_root_model()
-        output = model({"TDWI": tdwi})
-        twrt = output[output_name]
-        loss = twrt.sum()
+        output = model({param_name: param})
+        loss = output[output_name].sum()
 
-        # this is ∂loss/∂tdwi, for comparison with numerical gradient
-        grads = torch.autograd.grad(loss, tdwi, retain_graph=True)[0]
+        # this is ∂loss/∂param, for comparison with numerical gradient
+        grads = torch.autograd.grad(loss, param, retain_graph=True)[0]
 
-        # in this test, grads is very small
-        assert_array_almost_equal(numerical_grad, grads.item(), decimal=3)
+        assert_array_almost_equal(numerical_grad, grads.detach().numpy(), decimal=3)
+
+        # Warn if gradient is zero
+        if torch.all(grads == 0):
+            warnings.warn(
+                f"Gradient for parameter '{param_name}' with respect to output"
+                + f"'{output_name}' is zero: {grads.detach().numpy()}",
+                UserWarning,
+            )
