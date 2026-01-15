651b8c33f4763a723101fbcd95dd3134d750f2e5
SCiarella <58949181+SCiarella@users.noreply.github.com>
2026-01-15 11:52:10 +0100

    Update src/diffwofost/physical_models/crop/assimilation.py
    Co-authored-by: SarahAlidoost <55081872+SarahAlidoost@users.noreply.github.com>

--- src/diffwofost/physical_models/crop/assimilation.py
+++ src/diffwofost/physical_models/crop/assimilation.py
@@ -189,7 +189,7 @@ class WOFOST72_Assimilation(SimulationObject):
     | AMAXTB | Max. leaf CO2 assimilation rate as function of DVS                 | TCr  | kg CO2 ha⁻¹ leaf h⁻¹               |
     | EFFTB  | Light use effic. single leaf as a function of daily mean temperature                  | TCr  | kg CO2 ha⁻¹ h⁻¹ /(J m⁻² s⁻¹)      |
     | KDIFTB | Extinction coefficient for diffuse visible light as function of DVS| TCr  | -                                  |
-    | TMPFTB | Reduction factor on AMAX as function of DTEMP                      | TCr  | -                                  |
+    | TMPFTB | Reduction factor on AMAX as function of daily mean temperature                      | TCr  | -                                  |
     | TMNFTB | Reduction factor on AMAX as function of 7-day TMIN average         | TCr  | -                                  |
 
     **Rate variables**
