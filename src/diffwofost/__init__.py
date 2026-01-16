fcebe4a6155138e4590083e14e84c435425545c9
SarahAlidoost <f.alidoost@esciencecenter.nl>
2026-01-16 15:22:05 +0100

    add imports to init

--- src/diffwofost/__init__.py
+++ src/diffwofost/__init__.py
@@ -5,6 +5,9 @@ from diffwofost.physical_models import utils
 from diffwofost.physical_models.crop import leaf_dynamics
 from diffwofost.physical_models.crop import phenology
 from diffwofost.physical_models.crop import root_dynamics
+from diffwofost.physical_models.crop import assimilation
+from diffwofost.physical_models.crop import partitioning
+
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
@@ -16,5 +19,7 @@ __all__ = [
     "leaf_dynamics",
     "root_dynamics",
     "phenology",
+    "assimilation",
+    "partitioning",
     "utils",
 ]
