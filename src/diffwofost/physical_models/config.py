b4476321f75f3884b582daabafe0ec0a81649d12
SCiarella <58949181+SCiarella@users.noreply.github.com>
2026-02-10 14:13:56 +0100

    Update src/diffwofost/physical_models/config.py
    Co-authored-by: SarahAlidoost <55081872+SarahAlidoost@users.noreply.github.com>

--- src/diffwofost/physical_models/config.py
+++ src/diffwofost/physical_models/config.py
@@ -61,6 +61,10 @@ class ComputeConfig:
         >>> model_cpu = WOFOST_Leaf_Dynamics(...)
         >>>
         >>> # model_gpu remains on cuda, model_cpu stays on cpu.
+        
+    Setting the model properties like model.device = torch.device("cpu") or
+    model.dtype = torch.float64 returns `AttributeError`. Always use
+    ComputeConfig.set_device(...) and ComputeConfig.set_dtype(...).
 
     **Resetting to Defaults:**
 
